f02b20090aba433dfce7c3e4861a19ace8db2cef
Altansukh T <altansukh@altansukh.com>
2021-04-22 15:33:05 +0800

    Update spec.js for simpler async

--- frontendApp/test/spec.js
+++ frontendApp/test/spec.js
@@ -2,11 +2,11 @@ const Application = require('spectron').Application
 const assert = require('assert')
 const electronPath = require('electron') // Require Electron from the binaries included in node_modules.
 const path = require('path')
-const chaiAsPromised = require("chai-as-promised")
-const chai = require("chai")
+// const chaiAsPromised = require("chai-as-promised")
+// const chai = require("chai")
 
-chai.should()
-chai.use(chaiAsPromised)
+// chai.should()
+// chai.use(chaiAsPromised)
 
 describe("Application launch test", function () {
   this.timeout(10000)
@@ -22,20 +22,11 @@ describe("Application launch test", function () {
 				ELECTRON_ENABLE_LOGGING: true,
 				ELECTRON_ENABLE_STACK_DUMPING: true,
 				NODE_ENV: "development",
-				PORT: 9515
 			},
-			chromeDriverArgs: ["--headless","--no-sandbox",
-					   "--disable-dev-shm-usage",
-					   "start-maximized",
-					   "disable-infobars",
-					   "--disable-extensions",
-					   "--disable-gpu"
-					  ],
 			startTimeout: 20000,
-			chromeDriverLogPath: '../chromedriverlog.txt'
   		});
 		
-		chaiAsPromised.transferPromiseness = this.app.transferPromiseness;
+		// chaiAsPromised.transferPromiseness = this.app.transferPromiseness;
 		return this.app.start();      
 	});
 
@@ -45,19 +36,17 @@ describe("Application launch test", function () {
 		}
 	});
 
-	describe("Launch", function () {
-		it('Shows an initial window', function () {
-			return this.app.client.getWindowCount().then(function (count) {
-				assert.equal(count, 1)
-				// Please note that getWindowCount() will return 2 if `dev tools` are opened.
-				// assert.equal(count, 2)
-			})
+	it('Shows an initial window', function () {
+		return this.app.client.getWindowCount().then(function (count) {
+			assert.equal(count, 1)
+			// Please note that getWindowCount() will return 2 if `dev tools` are opened.
+			// assert.equal(count, 2)
 		})
+	})
 
-		it('Displays a title', function () {
-			return this.app.client.browserWindow.getTitle().then(function (title) {
-				assert.equal(title, 'SmartRover - Home')
-			})
-		});
+	it('Displays a correct title', function () {
+		return this.app.client.browserWindow.getTitle().then(function (title) {
+			assert.equal(title, 'SmartRover - Home')
+		})
 	});
 });
