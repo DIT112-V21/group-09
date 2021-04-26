ea02023afb66b63e6cebe20663bad98b6e08037c
nikalc <gusnikalc@student.gu.se>
2021-04-26 19:27:18 +0800

    Implement manual control buttons
    Tasks:
    - Completed layout update for manual control page
    - Implement control pad buttons
    - Implement camera stream update
    
    Next steps:
    - Needs to refactor command console and serial feed
    - Refactor RGB conversion part
    
    Resolves #63

--- frontendApp/assets/js/renderer.js
+++ frontendApp/assets/js/renderer.js
@@ -1,29 +1,39 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
 const mqtt = require('mqtt')
 window.mqtt = mqtt
-
 let client = null
 
+var r = 0;
+var capturecount = 1;
+var ln = 0;
+var flag = 0;
+var xres = 0;
+var yres = 0;
+var canvas;
+var ctx;
+var imgData;
+
 const options = {
-  keepalive: 30,
-  protocolId: 'MQTT',
-  protocolVersion: 4,
-  clean: true,
-  reconnectPeriod: 1000,
-  connectTimeout: 30 * 1000,
-  will: {
-    topic: 'WillMsg',
-    payload: 'Connection Closed abnormally..!',
-    qos: 0,
-    retain: false
-  },
-  rejectUnauthorized: false
+	keepalive: 30,
+	protocolId: 'MQTT',
+	protocolVersion: 4,
+	clean: true,
+	reconnectPeriod: 1000,
+	connectTimeout: 30 * 1000,
+	will: {
+		topic: 'WillMsg',
+		payload: 'Connection Closed abnormally..!',
+		qos: 0,
+		retain: false
+	},
+	rejectUnauthorized: false,
+	connectUrl: "mqtt://aerostun.dev:1883",
+	username: "group09",
+  	password: "smartrover",
+	clientId: "clientId_Hes24TQfz",
+	defaultTopic: "/smartRover/#"
+	
 }
+
 const connectBtn = document.getElementById('connectBtn')
 const disconnectBtn = document.getElementById('disconnectBtn')
 const subBtn = document.getElementById('subBtn')
@@ -37,13 +47,9 @@ unsubBtn.addEventListener('click', onUnsub)
 sendBtn.addEventListener('click', onSend)
 
 function onConnect () {
-  const { host, port, clientId, username, password } = connection
-  const connectUrl = `mqtt://${host.value}:${port.value}`
-  options.clientId = clientId.value || `mqttjs_${Math.random().toString(16).substr(2, 8)}`
-  options.username = username.value
-  options.password = password.value
+  // const { host, port, clientId, username, password } = connection
   console.log('connecting mqtt client')
-  client = mqtt.connect(connectUrl, options)
+  client = mqtt.connect(options.connectUrl, options)
   client.on('error', (err) => {
     console.error('Connection error: ', err)
     client.end()
@@ -52,14 +58,42 @@ function onConnect () {
     console.log('Reconnecting...')
   })
   client.on('connect', () => {
-    console.log('Client connected:' + options.clientId)
-    connectBtn.innerText = 'Connected'
+	  console.log('Client connected:' + options.clientId)
+	  connectBtn.innerText = 'Connected'
   })
   client.on('message', (topic, message) => {
-    const msg = document.createElement('div')
-    msg.className = 'message-body'
-    msg.innerText = `${message.toString()}\nOn topic: ${topic}`
-    document.getElementById('article').appendChild(msg)
+	  if (topic.includes("camera")) {
+		  console.log("Camera")
+		  canvas = document.getElementById('cameraCanvas');
+		  ctx = canvas.getContext('2d', {alpha: false});
+		  
+		  const width = 320;
+		  const height = 240;
+		  
+		  canvas.setAttribute('width', width);
+    	  canvas.setAttribute('height', height);
+		  
+		  const imageData = ctx.getImageData(0, 0, width, height);
+		  const data = message;
+
+		  for (var x = 0; x < width; x++) {
+        		for (var y = 0; y < height; y++) {
+            		var i = (x + y * width) * 3;
+            		var r = message[i + 0];
+            		var g = message[i + 1];
+            		var b = message[i + 2];
+
+            		ctx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+            		ctx.fillRect(x, y, 1, 1);
+				}
+    		}
+
+	  } else {
+		const msg = document.createElement('div')
+		msg.className = 'message-body'
+		msg.innerText = `${message.toString()}\nOn topic: ${topic}`
+		document.getElementById('article').appendChild(msg)
+  	  }
   })
 }
 
