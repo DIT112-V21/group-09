beedac335828fa22e816ca19e18859b15dbeef9f
Altansukh T <altansukh@altansukh.com>
2021-05-09 03:01:42 +0800

    Implement Mars mod
    Completed:
    - Overall map size and hue
    - Lighting conditions good
    - Added various rocks
    - Boundaries adjusted and good
    
    Next:
    - Test and more test
    - Adjust or add more rocks
    
    Resolves #78

--- mars-mod/src/environments/mars/Mars.gd
+++ mars-mod/src/environments/mars/Mars.gd
@@ -1,9 +1,8 @@
 extends Spatial
 
 func _ready() -> void:
-	var timer: Timer = Timer.new()
-	timer.wait_time = 2
-	add_child(timer)
+	pass
+	
 	
 func init_cam_pos() -> Basis:
 	return $Camera.global_transform
