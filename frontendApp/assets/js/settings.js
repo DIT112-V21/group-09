87768be81006a4f463ed3e3257c02e89c29f079d
Kamila Y <gusyoska@student.gu.se>
2021-05-17 19:00:24 +0200

    Update data persistence
    * Update data persistence to the database
    
    * Update setting's layout
    
    Contributes with #64

--- frontendApp/assets/js/settings.js
+++ frontendApp/assets/js/settings.js
@@ -1,211 +1,241 @@
-('#makeEditable').SetEditable({ 
-    $addButton: ('#but_add')
-  });
 
+//mqttTable
+function addRow(mqttTable) {
+  var table = document.getElementById(mqttTable);
+  var rowCount = table.rows.length;
+  var row = table.insertRow(rowCount);
+  var checkBoxCell = row.insertCell(0);
+  var step = document.createElement("input");
+  step.type = "checkbox";
+  step.name="chkbox[]";
 
-  // TableToCSV(tabId, separator)
-var data = TableToCSV('makeEditable', ',');
+  checkBoxCell.appendChild(step);
+  
+ // var stepNumberCell = row.insertCell(1);
+  //stepNumberCell.innerHTML = rowCount ;
 
-('#makeEditable').SetEditable({ 
-    $addButton: ('#but_add'),
-    columnsEd: null  // Ex.: "1,2,3,4,5"
-  });
+  var distanceCell = row.insertCell(1);
+  var distance = document.createElement("input");
+  distance.type = "number";
+  distance.value = "0";
+  distance.step = "100";
+  distance.min = "0";
+  distance.name = "txtbox[]";
+  distance.className = "data-cell";
+  distanceCell.appendChild(distance);
 
-('#makeEditable').SetEditable({
-    onEdit: function() {}, 
-    onDelete: function() {}, 
-    onBeforeDelete: function() {},
-    onAdd: function() {} 
-  });
+  var speedCell = row.insertCell(2);
+  var speed = document.createElement("input");
+  speed.type = "number";
+  speed.value = "0";
+  speed.step = "10";
+  speed.min = "-100";
+  speed.max = "100";
+  speed.name = "txtbox[]";
+  speed.className = "data-cell";
+  speedCell.appendChild(speed);
 
+  var headingCell = row.insertCell(3);
+  var heading = document.createElement("input");
+  heading.type = "number";
+  heading.value = "0";
+  heading.step = "5";
+  heading.min = "0";
+  heading.max = "360";
+  heading.name = "txtbox[]";
+  heading.className = "data-cell";
+  headingCell.appendChild(heading);
 
+  var headingCell = row.insertCell(4);
+  var heading = document.createElement("input");
+  heading.type = "number";
+  heading.value = "0";
+  heading.step = "5";
+  heading.min = "0";
+  heading.max = "360";
+  heading.name = "txtbox[]";
+  heading.className = "data-cell";
+  headingCell.appendChild(heading);
 
+  var headingCell = row.insertCell(5);
+  var heading = document.createElement("input");
+  heading.type = "text";
+  heading.value = "Pending";
+  heading.step = "5";
+  heading.min = "0";
+  heading.max = "360";
+  heading.name = "txtbox[]";
+  heading.className = "data-cell";
+  headingCell.appendChild(heading);
+}
 
-
-
-  $(document).ready(function () {
-
-    // ------------------------------------------------------- //
-    // Custom Scrollbar
-    // ------------------------------------------------------ //
-
-    if ($(window).outerWidth() > 992) {
-        $("nav.side-navbar").mCustomScrollbar({
-            scrollInertia: 200
-        });
+function deleteRow(mqttTable) {
+  try {
+      var table = document.getElementById(mqttTable);
+      var rowCount = table.rows.length;
+  let n = 1;
+  
+      for (var i=0; i < rowCount; i++) {
+          var row = table.rows[i];
+          var chkbox = row.cells[0].childNodes[0];
+    
+    if (i > 0) {
+      let cell2 = row.cells[1];
+      cell2.innerHTML = n;
+      n++;
     }
-
-    // Main Template Color
-    var brandPrimary = '#33b35a';
-
-    // ------------------------------------------------------- //
-    // Side Navbar Functionality
-    // ------------------------------------------------------ //
-    $('#toggle-btn').on('click', function (e) {
-
-        e.preventDefault();
-
-        if ($(window).outerWidth() > 1194) {
-            $('nav.side-navbar').toggleClass('shrink');
-            $('.page').toggleClass('active');
-        } else {
-            $('nav.side-navbar').toggleClass('show-sm');
-            $('.page').toggleClass('active-sm');
-        }
-    });
-
-    // ------------------------------------------------------- //
-    // Tooltips init
-    // ------------------------------------------------------ //    
-
-    $('[data-toggle="tooltip"]').tooltip()
-
-    // ------------------------------------------------------- //
-    // Universal Form Validation
-    // ------------------------------------------------------ //
-
-    $('.form-validate').each(function() {  
-        $(this).validate({
-            errorElement: "div",
-            errorClass: 'is-invalid',
-            validClass: 'is-valid',
-            ignore: ':hidden:not(.summernote),.note-editable.card-block',
-            errorPlacement: function (error, element) {
-                // Add the `invalid-feedback` class to the error element
-                error.addClass("invalid-feedback");
-                //console.log(element);
-                if (element.prop("type") === "checkbox") {
-                    error.insertAfter(element.siblings("label"));
-                } 
-                else {
-                    error.insertAfter(element);
-                }
-            }
-        });
-    });
-    // ------------------------------------------------------- //
-    // Material Inputs
-    // ------------------------------------------------------ //
-
-    var materialInputs = $('input.input-material');
-
-    // activate labels for prefilled values
-    materialInputs.filter(function () {
-        return $(this).val() !== "";
-    }).siblings('.label-material').addClass('active');
-
-    // move label on focus
-    materialInputs.on('focus', function () {
-        $(this).siblings('.label-material').addClass('active');
-    });
-
-    // remove/keep label on blur
-    materialInputs.on('blur', function () {
-        $(this).siblings('.label-material').removeClass('active');
-
-        if ($(this).val() !== '') {
-            $(this).siblings('.label-material').addClass('active');
-        } else {
-            $(this).siblings('.label-material').removeClass('active');
-        }
-    });
-
-    // ------------------------------------------------------- //
-    // Jquery Progress Circle
-    // ------------------------------------------------------ //
-    var progress_circle = $("#progress-circle").gmpc({
-        color: brandPrimary,
-        line_width: 5,
-        percent: 80
-    });
-    progress_circle.gmpc('animate', 80, 3000);
-
-    // ------------------------------------------------------- //
-    // External links to new window
-    // ------------------------------------------------------ //
-
-    $('.external').on('click', function (e) {
-
-        e.preventDefault();
-        window.open($(this).attr("href"));
-    });
-
-    // ------------------------------------------------------ //
-    // For demo purposes, can be deleted
-    // ------------------------------------------------------ //
-
-    var stylesheet = $('link#theme-stylesheet');
-    $("<link id='new-stylesheet' rel='stylesheet'>").insertAfter(stylesheet);
-    var alternateColour = $('link#new-stylesheet');
-
-    if ($.cookie("theme_csspath")) {
-        alternateColour.attr("href", $.cookie("theme_csspath"));
-    }
-
-    $("#colour").change(function () {
-
-        if ($(this).val() !== '') {
-
-            var theme_csspath = 'css/style.' + $(this).val() + '.css';
-
-            alternateColour.attr("href", theme_csspath);
-
-            $.cookie("theme_csspath", theme_csspath, {
-                expires: 365,
-                path: document.URL.substr(0, document.URL.lastIndexOf('/'))
-            });
-
-        }
-
-        return false;
-    });
-
-});
-
-
-
-
-
-
-
-// Script to show modal alert box
-var modalWrap = null;
-const showModal = (title, description, yesBtnLabel = 'Yes', noBtnLabel = 'Cancel') => {
-  if (modalWrap !== null) {
-    modalWrap.remove();
+    
+          if(null != chkbox && true == chkbox.checked) {
+              table.deleteRow(i);
+              rowCount--;
+              i--;
+      n--;
+          }
+      }
+  } catch(e) {
+      alert(e);
   }
+}
 
-  modalWrap = document.createElement('div');
-  modalWrap.innerHTML = `
-    <div class="modal fade" id="warningPower">
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-          <div class="modal-header bg-warning">
-            <h5 class="modal-title"><strong>${title}</strong></h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <p>${description}</p>
-          </div>
-          <div class="modal-footer bg-light">
-            <button type="button" class="btn btn-warning" data-bs-dismiss="modal">${noBtnLabel}</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  `;
-
-  document.body.append(modalWrap);
-
-  var modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
-  modal.show();
+function deleteRow(mqttTable) {
+  try {
+      var table = document.getElementById(mqttTable);
+      var rowCount = table.rows.length;
+  let n = 1;
+  
+      for (var i=0; i < rowCount; i++) {
+          var row = table.rows[i];
+          var chkbox = row.cells[0].childNodes[0];
+    
+    if (i > 0) {
+      let cell2 = row.cells[1];
+      cell2.innerHTML = n;
+      n++;
+    }
+    
+          if(null != chkbox && true == chkbox.checked) {
+              table.deleteRow(i);
+              rowCount--;
+              i--;
+      n--;
+          }
+      }
+  } catch(e) {
+      alert(e);
+  }
 }
 
 
-var consoleCommands = document.getElementById('showConsoleCommands')
-consoleCommands.addEventListener('click', function(){onShowConsoleCommands(); }, false)
-function onShowConsoleCommands() {
-    
-        var desc = "somethings"
-        showModal("Console Commands Adjustments", desc, yesBtnLabel = 'Yes', noBtnLabel = 'Close', false)
+const $tableID = $('#table');
+const $BTN = $('#export-btn'); 
+const $EXPORT = $('#export');
+const newTr = `
+<tr class="hide">
+  <td class="pt-3-half" contenteditable="true">Example</td>
+  <td class="pt-3-half" contenteditable="true">Example</td>
+  <td class="pt-3-half" contenteditable="true">Example</td>
+  <td class="pt-3-half" contenteditable="true">Example</td>
+  <td class="pt-3-half" contenteditable="true">Example</td>
+  <td class="pt-3-half">
+    <span class="table-up"
+      ><a href="#!" class="indigo-text"
+        ><i class="fas fa-long-arrow-alt-up" aria-hidden="true"></i></a
+    ></span>
+    <span class="table-down"
+      ><a href="#!" class="indigo-text"
+        ><i class="fas fa-long-arrow-alt-down" aria-hidden="true"></i></a
+    ></span>
+  </td>
+  <td>
+    <span class="table-remove"
+      ><button
+        type="button"
+        class="btn btn-danger btn-rounded btn-sm my-0 waves-effect waves-light"
+      >
+        Remove
+      </button></span
+    >
+  </td>
+</tr>
+`;
+ (table-add).on('click', 'i', () => { const $clone = $tableID.find('mqttTable2').last().clone(true).removeClass('hide table-line'); if ($tableID.find('mqttTable2').length
+=== 0) { $('tbody').append(newTr); } $tableID.find('table').append($clone); });
+
+$tableID.on('click', '.table-remove', function () { $(this).parents('tr').detach(); });
+
+$tableID.on('click', '.table-up', function () { const $row = $(this).parents('tr');if
+($row.index() === 0) { return; } $row.prev().before($row.get(0)); }); 
+
+$tableID.on('click','.table-down', function () { const $row = $(this).parents('tr');
+$row.next().after($row.get(0)); }); // A few jQuery helpers for exporting only jQuery.fn.pop= [].pop; 
+jQuery.fn.shift = [].shift; $BTN.on('click', () => { const $rows =
+
+$tableID.find('tr:not(:hidden)'); const headers = []; const data = []; // Get the headers("add special header logic here") 
+$($rows.shift()).find('th:not(:empty)').each(function () {
+headers.push($(this).text().toLowerCase()); }); // Turn all existing rows into a loopable array
+$rows.each(function () { const $td = $(this).find('td'); const h = {}; // Use theheaders from earlier to name our hash keys headers.
+
+forEach((header, i) => { h[header] =
+$td.eq(i).text(); }); data.push(h); }); // Output the result
+$EXPORT.text(JSON.stringify(data)); });
+
+
+
+
+
+
+//mqttTable2
+function addRow(mqttTable2) {
+  var table = document.getElementById(mqttTable2);
+  var rowCount = table.rows.length;
+  var row = table.insertRow(rowCount);
+  var checkBoxCell = row.insertCell(0);
+  var step = document.createElement("input");
+  step.type = "checkbox";
+  step.name="chkbox[]";
+
+  checkBoxCell.appendChild(step);
+  
+ // var stepNumberCell = row.insertCell(1);
+  //stepNumberCell.innerHTML = rowCount ;
+
+  var distanceCell = row.insertCell(1);
+  var distance = document.createElement("input");
+ 
+  distanceCell.appendChild(distance);
+
+  var speedCell = row.insertCell(2);
+  var speed = document.createElement("input");
+  
+  speedCell.appendChild(speed);
+
+  var headingCell = row.insertCell(3);
+  var heading = document.createElement("input");
+  
+  headingCell.appendChild(heading);
+
+  var headingCell = row.insertCell(4);
+  var heading = document.createElement("input");
+ 
+  headingCell.appendChild(heading);
+
+  var headingCell = row.insertCell(5);
+  var heading = document.createElement("input");
+ 
+  headingCell.appendChild(heading);
+}
+
+
+
+
+const saveBtn = document.getElementById('saveBtn')
+const cacelBtn = document.getElementById('cancelBtn')
+
+saveBtn.addEventListener('click', function(){persistData(saveBtn);} )
+
+
+function persistData(elmnt){
+
+
+
 }
