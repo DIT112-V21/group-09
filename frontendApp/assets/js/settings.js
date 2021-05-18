f05e21dcb9922c894539b2bc808eda49137fd2f7
Altansukh T <altansukh@altansukh.com>
2021-05-19 04:09:40 +0800

    Update settings and help
    Updating settings and help page layouts
    
    Contributes #64

--- frontendApp/assets/js/settings.js
+++ frontendApp/assets/js/settings.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 //mqttTable
 function addRow(mqttTable) {
@@ -237,5 +238,26 @@ saveBtn.addEventListener('click', function(){persistData(saveBtn);} )
 function persistData(elmnt){
 
 
+=======
+let config = require('electron-node-config');
+
+const mqttSaveBtn = document.getElementById('mqttSaveBtn')
+const mqttReloadBtn = document.getElementById('mqttReloadBtn')
+
+document.addEventListener("DOMContentLoaded", function() { loadSettings(); }, false);
+mqttSaveBtn.addEventListener('click', function(){ console.log("Saving..."); }, false)
+mqttReloadBtn.addEventListener('click', function(){ loadSettings(); }, false)
+
+function loadSettings() {
+	let mqttConfig = config.get('mqtt.brokerConfig');
+	
+	document.getElementById("mqtt-hostname").value = mqttConfig.host;
+	document.getElementById("mqtt-port").value = mqttConfig.port;
+	document.getElementById("mqtt-username").value = mqttConfig.username;
+	document.getElementById("mqtt-password").value = mqttConfig.password;
+	document.getElementById("mqtt-clientid").value = mqttConfig.clientId;
+	document.getElementById("mqtt-receive-topic").value = mqttConfig.receivingChannel;
+	document.getElementById("mqtt-publish-topic").value = mqttConfig.sendingChannel;
+>>>>>>> Stashed changes
 
 }
