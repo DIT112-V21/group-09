6ffd875212a4d0bf7fea0e6d135fbbe810207664
Altansukh T <altansukh@altansukh.com>
2021-05-29 18:14:46 +0800

    Prepare beta release config
    v0.4.1
    - Various bugfixes
    - Update config setup
    - Add electronbuilder
    - New electron build workflow
    - Changelog generator only on release branches
    
    Resolves #111

--- frontendApp/assets/js/missionControl.js
+++ frontendApp/assets/js/missionControl.js
@@ -1,6 +1,6 @@
 const mqtt = require('mqtt')
 let config = require('electron-node-config');
-let mqttConfig = config.get('mqtt.brokerConfig');
+let mqttConfig = = store.get('localMqttSettings');
 const QrScanner = require('./assets/js/qr-scanner.umd.min.js');
 QrScanner.WORKER_PATH = './assets/js/qr-scanner-worker.min.js';
 
@@ -38,100 +38,104 @@ const options = {
 
 const powerBtn = document.getElementById('powerBtn')
 const streamBtn = document.getElementById('streamBtn')
+const sendBtn = document.getElementById('sendBtn')
 
 powerBtn.addEventListener('click', function(){ onConnect() }, false)
 streamBtn.addEventListener('click', function(){ onSub() }, false)
+sendBtn.addEventListener('click', function(){ sendMission() }, false)
 
-async function onConnect() {
+function onConnect() {
   // const { host, port, clientId, username, password } = connection
 	if (client == null || !client.connected) {
-		console.log('connecting mqtt client')
-		  client = mqtt.connect(options.connectUrl, options)
-		  client.on('error', (err) => {
-			console.error('Connection error: ', err)
+		console.log('connecting mqtt client');
+		client = mqtt.connect(options.connectUrl, options)
+		client.on('error', (err) => {
+			console.error('Connection error: ', err);
 			client.end()
-		  })
-		  client.on('reconnect', () => {
+		})
+		client.on('reconnect', () => {
 			console.log('Reconnecting...')
-		  })
-		  client.on('connect', () => {
-				console.log('Client connected:' + options.clientId)
-			  	addTextToSerial('Rover powered up!');
-				// connectBtn.innerText = 'Connected';
-			  	updateTimestamp();
-				document.getElementById("powerbutton").classList.remove('disconnected');
-				document.getElementById("powerbutton").classList.add('connected');
-				document.getElementById("powerbutton").style.setProperty('--power-btn-color', '#18ff00', 'important');
-			  	document.getElementById("nasaStream").style.setProperty('display', 'none', 'important')
-			    document.getElementById("roverStream").style.setProperty('display', 'flex', 'important')
-			  	//document.getElementById("streamButton").style.setProperty('color', 'red', 'important');
-		  })
+		})
+		client.on('connect', () => {
+			console.log('Client connected:' + options.clientId)
+			addTextToSerial('Rover powered up!');
+			
+			updateTimestamp();
+			document.getElementById("powerbutton").classList.remove('disconnected');
+			document.getElementById("powerbutton").classList.add('connected');
+			document.getElementById("powerbutton").style.setProperty('--power-btn-color', '#18ff00', 'important');
+			document.getElementById("nasaStream").style.setProperty('display', 'none', 'important')
+			document.getElementById("roverStream").style.setProperty('display', 'flex', 'important')
+			document.getElementById("streamButton").style.setProperty('color', 'red', 'important');
+	  	})
 		client.on('message', (topic, message) => {
-			  if (topic.includes("mission/status")) {
-				  addTextToSerial(message);
-			  } else if (topic.includes("mission/step")) {
-				  addTextToSerial(message);
-			  } else if (topic.includes("camera")) {
-				  console.log("Camera")
-				  canvas = document.getElementById('missionStream');
-				  ctx = canvas.getContext('2d');
+			if (topic.includes("mission/status")) {
+				addTextToSerial(message);
+			} else if (topic.includes("mission/step")) {
+				addTextToSerial(message);
+			} else if (topic.includes("rover/ready")) {
+				sendMission();
+				document.getElementById("sendbutton").style.setProperty('color', 'orange', 'important');
+			} else if (topic.includes("camera")) {
+				console.log("Camera")
+				canvas = document.getElementById('missionStream');
+				ctx = canvas.getContext('2d');
+				 
+				const width = 640;
+				const height = 480;
+				const arrayBuffer = new ArrayBuffer(width * height * 4);
+				const pixels = new Uint8ClampedArray(arrayBuffer);
+				var n = 0;	
 				  
-				  const width = 640;
-				  const height = 480;
-				  const arrayBuffer = new ArrayBuffer(width * height * 4);
-				  const pixels = new Uint8ClampedArray(arrayBuffer);
-				  var n = 0;	
+				for (var i = 0; i < message.length; i += 3) {
+					pixels[n] = message[i];
+					pixels[n + 1] = message[i + 1];
+					pixels[n + 2] = message[i + 2];
+					pixels[n + 3] = 255;
+					n += 4;
+				}
+				const imageData = new ImageData(pixels, width, height);
+				ctx.putImageData(imageData, 0, 0);
 				  
-				  for (var i = 0; i < message.length; i += 3) {
-						n += 4;
-						pixels[n] = message[i];
-						pixels[n + 1] = message[i + 1];
-						pixels[n + 2] = message[i + 2];
-						pixels[n + 3] = 255;
-				  }
-				  const imageData = new ImageData(pixels, width, height);
-				  ctx.putImageData(imageData, 0, 0);
+				frameCount++;
 				  
-				  frameCount++;
-				  
-				  if (frameCount > 20) {
-					  var scanData = canvas.toDataURL('image/bmp');
-					  QrScanner.scanImage(scanData)
-						  .then(result => {
-						  	var qrMessage = "Found target area: " + result;
+				if (frameCount > 20) {
+					var scanData = canvas.toDataURL('image/bmp');
+					QrScanner.scanImage(scanData)
+						.then(result => {
+							var qrMessage = "Found target area: " + result;
 						  	addTextToSerial(qrMessage);
 						  
 						    endMission();
 						  
-						  })
-						  .catch(error => console.log(error || 'No QR code found.'));
-					  frameCount = 0;
-				  }
+						 })
+						 .catch(error => console.log(error || 'No QR code found.'));
+					frameCount = 0;
+				}
 				  
-			  } else {
+			} else {
 				const msg = `${message.toString()}\nOn topic: ${topic}`
-					addTextToSerial(msg);
-			  }
-		  })
+				addTextToSerial(msg);
+			}
+		})
 		
 		return client.connected = true;
 		  
 	  } else if (client.connected) {
-		  onUnsub();
-		  client.end()
-		  client.on('close', () => {
-			  console.log(options.clientId + ' disconnected')
-    		})
-		  addTextToSerial('Rover turned off!');
-		  document.getElementById("powerbutton").classList.add('disconnected');
-		  document.getElementById("powerbutton").classList.remove('connected');
-		  document.getElementById("powerbutton").style.setProperty('--power-btn-color', '#ff0000', 'important');
-		  document.getElementById("nasaStream").style.setProperty('display', 'flex', 'important')
-		  document.getElementById("roverStream").style.setProperty('display', 'none', 'important')
-		  document.getElementById("streamButton").style.setProperty('color', '#3A474D', 'important');
-		  
-		  return client.connected = false;
-	  }
+		onUnsub();
+		client.end()
+		client.on('close', () => {
+			console.log(options.clientId + ' disconnected')
+    	})
+		addTextToSerial('Rover turned off!');
+		document.getElementById("powerbutton").classList.add('disconnected');
+		document.getElementById("powerbutton").classList.remove('connected');
+		document.getElementById("powerbutton").style.setProperty('--power-btn-color', '#ff0000', 'important');
+		document.getElementById("nasaStream").style.setProperty('display', 'flex', 'important')
+		document.getElementById("roverStream").style.setProperty('display', 'none', 'important')
+		document.getElementById("streamButton").style.setProperty('color', '#3A474D', 'important');
+		return client.connected = false;
+	}
 }
 
 function onDisconnect () {
@@ -145,7 +149,7 @@ function onDisconnect () {
   }
 }
 
-async function onSub () {
+function onSub () {
 	if ((client == null || !client.connected || client.connected == false)) {
 		var desc = "The SmartRover is not powered up!<br />Please turn on Rover by pressing Power button."
 		saveModal("Rover power", desc, noBtnLabel = 'Close', false)
@@ -195,33 +199,18 @@ function onUnsub () {
 }
 
 
-function sendMission(content) {
-	
-	onConnect()
-	.catch((e) =>
-		console.log(e)
-	);
-	
-	onSub()
-		.catch((e) =>
-		console.log(e)
-	);
-	
-	if (subscribed) {
-		document.getElementById("streamButton").classList.add('connected');
-		document.getElementById("streamButton").classList.remove('disconnected');
-		document.getElementById("streamButton").style.removeProperty('color');
-		document.getElementById("streamButton").style.setProperty('color', '#18ff00', 'important');
-	}
-	
+function sendMission() {
 	if ((client == null || !client.connected || client.connected == false)) {
-    		var desc = "The <strong>SmartRover</strong> is not powered up!<br />Please turn on Rover by pressing Power button.<br />Also please connect to &quot;Mars Orbiter&quot; satellite for updates."
-			saveModal("Rover power", desc, noBtnLabel = 'Close', false) 
+    		var desc = "The <strong>SmartRover</strong> is not powered up!<br />Please turn on Rover by pressing Power button.<br />Also please connect to &quot;Mars Orbiter&quot; satellite for updates.<br /><br />Once everything is ready, click &quot;Send mession&quot; button." 
+			saveModal("Rover power", desc, noBtnLabel = 'Close', false); 
+	} else {
+		
+		var messageContent = store.get('missionContent');
+		if (messageContent == null) {
+			var descNoContent = "Cannot load mission content. Please make sure your mission is properly setup either on the map or table. Once ready, click &quot;Execute mission&quot; to process and send mission content."
+			saveModal("Mission setup", descNoContent, noBtnLabel = 'Close', false);
 		} else {
-			
-			var missionSteps = content.steps.length;
-			var messageContent = '';
-
+			var missionSteps = messageContent.content.steps.length;
 			for (var i = 0; i < missionSteps; i++) {
 				var step = 	String(i) + ';' + 
 							String(content.steps[i].parameters.heading) + ';' + 
@@ -229,9 +218,9 @@ function sendMission(content) {
 							String(content.steps[i].parameters.distance) + ';' 
 				messageContent += step;
 			}
-			
+
 			console.log(messageContent);
-			
+
 			client.publish( 'mission/content', messageContent, {
 				qos: 2,
 				retain: false
@@ -242,6 +231,7 @@ function sendMission(content) {
 			})
 			addTextToSerial('Mission successfully sent!');
 		}
+	}
 }
 
 function endMission() {
@@ -257,8 +247,6 @@ function endMission() {
 		var missionEndDesc = "Mission had completed. We have reached a target area and detected target code.<br /><br />Congratulations!";
 		saveModal('Mission complete', missionEndDesc, 'Close');	
 	}
-	
-	
 }
 
 function addTextToSerial(textToAdd) {
